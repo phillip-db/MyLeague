3439bb438f1f7d22f9e7d70c7597c6e411469648
Phillip Burleigh <pburle2@illinois.edu>
2021-04-21 15:10:08 -0500

    Configure .gitignore, add main helper method to read API Key from file

--- apps/my_league_main.cc
+++ apps/my_league_main.cc
@@ -1,3 +1,20 @@
+#include <handler.h>
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+std::string ReadAPIKey() {
+  std::ifstream api_file("data/no_commit_api_key.txt");
+  std::string api_key;
+
+  getline(api_file, api_key);
+  api_file.close();
+
+  return api_key;
+}
+
 int main() {
+  std::cout << ReadAPIKey() << std::endl;
   return 0;
 }
